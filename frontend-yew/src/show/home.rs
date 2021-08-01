0e67386f5c35c20b5f10ff3831c5aeefe097eb24
zzy <9809920@qq.com>
2021-08-01 09:17:46 +0800

    -

--- frontend-yew/src/show/home.rs
+++ frontend-yew/src/show/home.rs
@@ -42,7 +42,7 @@ impl Component for Home {
         match &self.data {
             FetchState::NotFetching => html! { "NotFetching" },
             FetchState::Fetching => html! { "Fetching" },
-            FetchState::Success(data) => self.view_data(data),
+            FetchState::Success(home_data) => view_home(home_data),
             FetchState::Failed(err) => html! { err },
         }
     }
@@ -57,6 +57,7 @@ impl Component for Home {
         match msg {
             Msg::SetState(fetch_state) => {
                 self.data = fetch_state;
+
                 true
             }
             Msg::GetData => {
@@ -66,7 +67,9 @@ impl Component for Home {
                         Err(err) => Msg::SetState(FetchState::Failed(err)),
                     }
                 });
+
                 self.link.send_message(Msg::SetState(FetchState::Fetching));
+
                 false
             }
         }
@@ -79,121 +82,119 @@ impl Component for Home {
     }
 }
 
-impl Home {
-    fn view_data(&self, data: &Value) -> Html {
-        let wish_val = &data["randomWish"];
-        let random_wish = html! {
-            <div class="ta-center mt16 mx32">
-                <b>
-                    <a href={ format!("/{}", wish_val["user"]["username"].as_str().unwrap()) }
-                        target="_blank">
-                        { wish_val["user"]["nickname"].as_str().unwrap() }
-                        { "@" }
-                        { wish_val["user"]["blogName"].as_str().unwrap() }
-                    </a>
-                    { " shared the aphorism: " }
-                </b>
-                { wish_val["aphorism"].as_str().unwrap() }
-                { " -- " }
-                { wish_val["author"].as_str().unwrap() }
-            </div>
-        };
+fn view_home(home_data: &Value) -> Html {
+    let wish_val = &home_data["randomWish"];
+    let random_wish = html! {
+        <div class="ta-center mt16 mx32">
+            <b>
+                <a href={ format!("/{}", wish_val["user"]["username"].as_str().unwrap()) }
+                    target="_blank">
+                    { wish_val["user"]["nickname"].as_str().unwrap() }
+                    { "@" }
+                    { wish_val["user"]["blogName"].as_str().unwrap() }
+                </a>
+                { " shared the aphorism: " }
+            </b>
+            { wish_val["aphorism"].as_str().unwrap() }
+            { " -- " }
+            { wish_val["author"].as_str().unwrap() }
+        </div>
+    };
 
-        let top_articles_vec = data["topArticles"].as_array().unwrap();
-        let top_articles = top_articles_vec.iter().map(|top_article| {
-            let article_topics_vec = top_article["topics"].as_array().unwrap();
-            let article_topics = article_topics_vec.iter().map(|topic| {
-                html! {
-                    <a class="s-badge s-badge__sm ml4 mb2"
-                        href={ topic["uri"].as_str().unwrap().to_owned() } target="_blank">
-                        { topic["name"].as_str().unwrap() }
-                    </a>
-                }
-            });
-
+    let top_articles_vec = home_data["topArticles"].as_array().unwrap();
+    let top_articles = top_articles_vec.iter().map(|top_article| {
+        let article_topics_vec = top_article["topics"].as_array().unwrap();
+        let article_topics = article_topics_vec.iter().map(|topic| {
             html! {
-                <div class="s-card flex--item fl-equal wmn3 m6">
-                    <h2 class="mb6">
-                        <a class="s-tag mr6"
-                            href={ top_article["category"]["uri"].as_str().unwrap().to_owned() }
-                            target="_blank">
-                            { top_article["category"]["name"].as_str().unwrap() }
-                        </a>
-                        <a href={ top_article["uri"].as_str().unwrap().to_owned() } target="_blank">
-                            { top_article["subject"].as_str().unwrap() }
-                        </a>
-                    </h2>
-                    <p class="fs-caption my6">
-                        { top_article["updatedAt"].as_str().unwrap() }
-                        { " by " }
-                        <a href={ format!("/{}", top_article["user"]["username"].as_str().unwrap()) }
-                            target="_blank">
-                            { top_article["user"]["nickname"].as_str().unwrap() }
-                            { "@" }
-                            { top_article["user"]["blogName"].as_str().unwrap() }
-                        </a>
-                    </p>
-                    <p class="my6">
-                        <b>{ "Topics:" }</b>
-                        { for article_topics }
-                    </p>
-                    <p class="fs-body1 v-truncate3 mt6">{ top_article["summary"].as_str().unwrap() }</p>
-                </div>
-            }
-        });
-
-        let recommended_articles_vec =
-            data["recommendedArticles"].as_array().unwrap();
-        let recommended_articles = recommended_articles_vec.iter().map(|recommended_article| {
-            let article_topics_vec = recommended_article["topics"].as_array().unwrap();
-            let article_topics = article_topics_vec.iter().map(|topic| {
-                html! {
-                    <a class="s-badge s-badge__sm ml4 mb2"
-                        href={ topic["uri"].as_str().unwrap().to_owned() } target="_blank">
-                        { topic["name"].as_str().unwrap() }
-                    </a>
-                }
-            });
-
-            html! {
-                <div class="s-card mx24 my6 sm:mx16">
-                    <h2 class="mb6">
-                        <a class="s-tag mr6"
-                            href={ recommended_article["category"]["uri"].as_str().unwrap().to_owned() }
-                            target="_blank">
-                            { recommended_article["category"]["name"].as_str().unwrap() }
-                        </a>
-                        <a href={ recommended_article["uri"].as_str().unwrap().to_owned() } target="_blank">
-                            { recommended_article["subject"].as_str().unwrap() }
-                        </a>
-                    </h2>
-                    <p class="fs-caption my6">
-                        { recommended_article["updatedAt"].as_str().unwrap() }
-                        { " by " }
-                        <a href={ format!("/{}", recommended_article["user"]["username"].as_str().unwrap()) }
-                            target="_blank">
-                            { recommended_article["user"]["nickname"].as_str().unwrap() }
-                            { "@" }
-                            { recommended_article["user"]["blogName"].as_str().unwrap() }
-                        </a>
-                    </p>
-                    <p class="my6">
-                        <b>{ "Topics:" }</b>
-                        { for article_topics }
-                    </p>
-                    <p class="fs-body1 v-truncate3 mt6">{ recommended_article["summary"].as_str().unwrap() }</p>
-                </div>
+                <a class="s-badge s-badge__sm ml4 mb2"
+                    href={ topic["uri"].as_str().unwrap().to_owned() } target="_blank">
+                    { topic["name"].as_str().unwrap() }
+                </a>
             }
         });
 
         html! {
-            <>
-                { random_wish }
-                <div class="d-flex gs32 fw-wrap p32 sm:p24">
-                    { for top_articles }
-                </div>
-                { for recommended_articles }
-            </>
+            <div class="s-card flex--item fl-equal wmn3 m6">
+                <h2 class="mb6">
+                    <a class="s-tag mr6"
+                        href={ top_article["category"]["uri"].as_str().unwrap().to_owned() }
+                        target="_blank">
+                        { top_article["category"]["name"].as_str().unwrap() }
+                    </a>
+                    <a href={ top_article["uri"].as_str().unwrap().to_owned() } target="_blank">
+                        { top_article["subject"].as_str().unwrap() }
+                    </a>
+                </h2>
+                <p class="fs-caption my6">
+                    { top_article["updatedAt"].as_str().unwrap() }
+                    { " by " }
+                    <a href={ format!("/{}", top_article["user"]["username"].as_str().unwrap()) }
+                        target="_blank">
+                        { top_article["user"]["nickname"].as_str().unwrap() }
+                        { "@" }
+                        { top_article["user"]["blogName"].as_str().unwrap() }
+                    </a>
+                </p>
+                <p class="my6">
+                    <b>{ "Topics:" }</b>
+                    { for article_topics }
+                </p>
+                <p class="fs-body1 v-truncate3 mt6">{ top_article["summary"].as_str().unwrap() }</p>
+            </div>
         }
+    });
+
+    let recommended_articles_vec =
+        home_data["recommendedArticles"].as_array().unwrap();
+    let recommended_articles = recommended_articles_vec.iter().map(|recommended_article| {
+        let article_topics_vec = recommended_article["topics"].as_array().unwrap();
+        let article_topics = article_topics_vec.iter().map(|topic| {
+            html! {
+                <a class="s-badge s-badge__sm ml4 mb2"
+                    href={ topic["uri"].as_str().unwrap().to_owned() } target="_blank">
+                    { topic["name"].as_str().unwrap() }
+                </a>
+            }
+        });
+
+        html! {
+            <div class="s-card mx24 my6 sm:mx16">
+                <h2 class="mb6">
+                    <a class="s-tag mr6"
+                        href={ recommended_article["category"]["uri"].as_str().unwrap().to_owned() }
+                        target="_blank">
+                        { recommended_article["category"]["name"].as_str().unwrap() }
+                    </a>
+                    <a href={ recommended_article["uri"].as_str().unwrap().to_owned() } target="_blank">
+                        { recommended_article["subject"].as_str().unwrap() }
+                    </a>
+                </h2>
+                <p class="fs-caption my6">
+                    { recommended_article["updatedAt"].as_str().unwrap() }
+                    { " by " }
+                    <a href={ format!("/{}", recommended_article["user"]["username"].as_str().unwrap()) }
+                        target="_blank">
+                        { recommended_article["user"]["nickname"].as_str().unwrap() }
+                        { "@" }
+                        { recommended_article["user"]["blogName"].as_str().unwrap() }
+                    </a>
+                </p>
+                <p class="my6">
+                    <b>{ "Topics:" }</b>
+                    { for article_topics }
+                </p>
+                <p class="fs-body1 v-truncate3 mt6">{ recommended_article["summary"].as_str().unwrap() }</p>
+            </div>
+        }
+    });
+
+    html! {
+        <>
+            { random_wish }
+            <div class="d-flex gs32 fw-wrap p32 sm:p24">
+                { for top_articles }
+            </div>
+            { for recommended_articles }
+        </>
     }
 }
